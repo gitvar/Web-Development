b25e65601b977ba7043fcea8fdb1ea011cf23705
gitvar <benvanderm@gmail.com>
2017-04-11 23:37:37 +0200

    First commit.

--- Sinatra/book-viewer/book_viewer.rb
+++ Sinatra/book-viewer/book_viewer.rb
@@ -1,6 +1,18 @@
 require "sinatra"
 require "sinatra/reloader"
+require "tilt/erubis"
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @contents = File.readlines("data/toc.txt") # readlines
+  @chapter = File.read("data/chp1.txt") # read
+
+  erb :chapter
+end
 
 get "/" do
-  File.read "public/template.html"
+  @title = "Sherlock Holmes"
+  @contents = File.readlines('data/toc.txt')
+
+  erb :home
 end

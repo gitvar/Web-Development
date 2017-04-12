7e11b321cf4bf90dd76b31937dc429442b4804c2
gitvar <benvanderm@gmail.com>
2017-04-12 19:08:17 +0200

    Changes and new commits.

--- Sinatra/dynamic-index/list.rb
+++ Sinatra/dynamic-index/list.rb
@@ -4,6 +4,7 @@ require "sinatra/reloader"
 require "tilt/erubis"
 
 get "/" do
+  # @files = Dir.glob("public/*").map { |file| File.basename(file) }.sort
   @files = Dir.glob("public/*").select { |file| File.ftype(file) == "file" }
   @files = @files.map { |file| File.basename(file) }.sort
   @files.reverse! if params[:sort] == "desc"

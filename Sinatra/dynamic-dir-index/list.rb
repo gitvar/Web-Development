f9668c1c628e23e31ee564ba01e81aa681eb4036
gitvar <benvanderm@gmail.com>
2017-04-21 21:38:11 +0200

    Fix the 'Dir.glob' command to find ONLY filenames (No prepended directory names and no directory names either).

--- Sinatra/dynamic-dir-index/list.rb
+++ Sinatra/dynamic-dir-index/list.rb
@@ -4,8 +4,13 @@ require "sinatra/reloader"
 require "tilt/erubis"
 
 get "/" do
-  @files = Dir.glob("public/*").select { |file| File.ftype(file) == "file" }
-  @files = @files.map { |file| File.basename(file) }.sort
+  # Find ONLY the filenames in the 'public' directory.
+  # No prepended directory names like 'public/demo1.html'.
+  # And no directory names (if there are directories in 'public').
+  @files = Dir.glob("public/*").map do |file|
+    File.basename(file) if File.ftype(file) == "file"
+  end.compact.sort
+
   @sort = params[:sort]
   @files.reverse! if params[:sort] == "desc"
 

52da873c0c825eb3bf66a196faeab6bac7ebe77e
gitvar <benvanderm@gmail.com>
2017-04-24 09:34:26 +0200

    Changes, deletions and new commits.

--- Sinatra/book-viewer-1/book_viewer.rb
+++ Sinatra/book-viewer-1/book_viewer.rb
@@ -2,18 +2,33 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
-get "/" do
+before do
   @title = "The Adventures of Sherlock Holmes"
   @author = "Sir Arthur Conan Doyle"
-  @contents = File.readlines('data/toc.txt')
+  @contents = File.readlines("data/toc.txt")
+end
 
+helpers do
+  def insert_paragraphs(text)
+    text.split("\n\n").map do |paragraph|
+      "<p>#{paragraph}</p>"
+    end.join
+  end
+end
+
+not_found do
+  redirect "/"
+end
+
+get "/" do
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "The Adventures of Sherlock Holmes"
-  @author = "Sir Arthur Conan Doyle"
-  @chapter = File.read('data/chp1.txt')
+get "/chapters/:number" do
+  number = params[:number].to_i
+  redirect "/" if number < 1 || number > @contents.size
+  @chapter_title = @contents[number - 1]
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
 end

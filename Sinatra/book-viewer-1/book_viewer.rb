7a7f4bf164d4849ed8933ecc7546ae8976552188
gitvar <benvanderm@gmail.com>
2017-05-03 06:14:47 +0200

    Added code to show search query in bold WHEN the chapter is displayed.

--- Sinatra/book-viewer-1/book_viewer.rb
+++ Sinatra/book-viewer-1/book_viewer.rb
@@ -5,17 +5,23 @@ require "tilt/erubis"
 before do
   @title = "The Adventures of Sherlock Holmes"
   @author = "Sir Arthur Conan Doyle"
-  @contents = File.readlines("data/toc.txt")
+  @table_of_contents = File.readlines("data/toc.txt")
 end
 
 helpers do
-  def insert_paragraphs(text)
-    # text.split("\n\n").map do |paragraph|
-    #   "<p>#{paragraph}</p>"
-    # end.join
-    text.split("\n\n").each_with_index.map do |paragraph, index|
-      "<p id=paragraph#{index}>#{paragraph}</p>"
+  def insert_paragraphs(chapter, query = "")
+    chapter.split("\n\n").each_with_index.map do |paragraph, index|
+      if query == ""
+        "<p id=paragraph#{index}>#{paragraph}</p>"
+      else
+        paragraph = highlight(paragraph, query)
+        "<p id=paragraph#{index}>#{paragraph}</p>"
+      end
     end.join
+  end
+
+  def highlight(text, query)
+    text.gsub(query, %(<strong>#{query}</strong>))
   end
 end
 
@@ -28,41 +34,48 @@ get "/" do
 end
 
 get "/chapters/:number" do
-  chap_number = params[:number].to_i
-  no_of_chapters = @contents.size
-  redirect "/" unless (1..no_of_chapters).to_a.include?(chap_number)
+  chapter_number = params[:number].to_i
+  no_of_chapters = @table_of_contents.size
+  redirect "/" unless (1..no_of_chapters).to_a.include?(chapter_number)
 
-  @chapter_title = @contents[chap_number - 1]
-  @chapter = File.read("data/chp#{chap_number}.txt")
+  @chapter_title = @table_of_contents[chapter_number - 1]
+  @chapter = File.read("data/chp#{chapter_number}.txt")
 
   erb :chapter
 end
 
 # Calls the block for each chapter, passing that chapter's number, name, and
-# contents.
+# the chapter contents.
 def each_chapter
-  @contents.each_with_index do |name, index|
-    number = index + 1
-    chapter = File.read("data/chp#{number}.txt")
-    yield number, name, chapter
+  @table_of_contents.each_with_index do |chapter_name, index|
+    chapter_number = index + 1
+    chapter_contents = File.read("data/chp#{chapter_number}.txt")
+    yield chapter_number, chapter_name, chapter_contents
   end
 end
 
-# This method returns an Array of Hashes representing chapters that match the
-# specified query. Each Hash contain values for its :name and :number keys.
-# results[0][:name] would provide the name of the first chapter matching the
-# search criteria.
+# This method returns an Array of Hashes representing chapters that match the specified query. Each Hash contain values for its :chapter_name, :chapter_number and paragraph keys. The :paragraph key points to another hash; the matching_paragraphs hash.
 def chapters_matching(query)
   results = []
 
   return results if query.nil? || query == ''
 
-  each_chapter do |number, name, chapter|
-    matches = {}
-    chapter.split("\n\n").each_with_index do |paragraph, index|
-      matches[index] = paragraph if paragraph.include?(query)
+  each_chapter do |chapter_number, chapter_name, chapter_contents|
+    matching_paragraphs = {}
+
+    # The 'matching_paragraphs' hash contains all the paragraphs (from each
+    # chapter) which contain the search query.
+    chapter_contents.split("\n\n").each_with_index do |paragraph, index|
+      matching_paragraphs[index] = paragraph if paragraph.include?(query)
     end
-    results << { number: number, name: name, paragraphs: matches } if matches.any?
+
+    # If there is at least one paragraph with text that match the search query,
+    # save the chapter number, the chapter name and the HASH of all the
+    # matching paragraphs to the results array.
+    if matching_paragraphs.any?
+      results << { number: chapter_number, name: chapter_name,
+      paragraphs: matching_paragraphs, query: query }
+    end
   end
 
   results
